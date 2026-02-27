0247380d7da94900339bfc174013e3887d317d6d
Italo Alves <italo@discourse.org>
2026-02-27 13:47:56 -0300

    DEV: Vendor CDN JS and CSS dependencies locally

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "alpine-turbo-drive-adapter", to: "https://cdn.jsdelivr.net/npm/alpine-turbo-drive-adapter@2.2.0/dist/alpine-turbo-drive-adapter.esm.js"
+pin "alpine-turbo-drive-adapter" # @2.2.0
 pin "alpinejs" # @3.15.8
-pin "flatpickr", to: "https://esm.sh/flatpickr@4.6.13"
+pin "flatpickr" # @4.6.13
