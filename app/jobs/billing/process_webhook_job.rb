d456611873dc5927d86e21e00a2a17cfe11b613f
Ítalo <18471038+italoaalves@users.noreply.github.com>
2026-03-05 07:04:13 -0300

    FIX: Add new status to purchase webhook (#24)

--- app/jobs/billing/process_webhook_job.rb
+++ app/jobs/billing/process_webhook_job.rb
@@ -4,6 +4,8 @@ module Billing
   class ProcessWebhookJob < ApplicationJob
     queue_as :default
 
+    retry_on StandardError, wait: :polynomially_longer, attempts: 5
+
     def perform(payload:)
       Billing::WebhookProcessor.call(payload)
     end
