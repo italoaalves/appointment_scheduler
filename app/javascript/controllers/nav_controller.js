7a477d8cc6ff7c775c902e9a8f1090557436fda1
Italo Alves <italoalves.software@gmail.com>
2026-04-02 13:25:44 -0300

    DEV: Use data-turbo-permanent on dock to persist flyout across navigation

--- app/javascript/controllers/nav_controller.js
+++ app/javascript/controllers/nav_controller.js
@@ -1,20 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
-const STORAGE_KEY = "nav_open_group"
-
 export default class extends Controller {
   static targets = ["flyout", "sheet", "sheetOverlay"]
-
-  connect() {
-    const openGroup = sessionStorage.getItem(STORAGE_KEY)
-    if (!openGroup) return
-
-    const panel = this.flyoutTargets.find(el => el.dataset.navGroup === openGroup)
-    if (panel) {
-      panel.classList.remove("hidden", "opacity-0", "translate-x-2")
-      panel.classList.add("opacity-100", "translate-x-0")
-    }
-  }
 
   toggle(event) {
     event.stopPropagation()
@@ -25,7 +12,6 @@ export default class extends Controller {
     const isOpen = !panel.classList.contains("hidden")
     this._closeFlyouts()
     if (!isOpen) {
-      sessionStorage.setItem(STORAGE_KEY, group)
       panel.classList.remove("hidden")
       requestAnimationFrame(() => {
         panel.classList.remove("opacity-0", "translate-x-2")
@@ -35,15 +21,7 @@ export default class extends Controller {
   }
 
   closeFlyout() {
-    sessionStorage.removeItem(STORAGE_KEY)
     this._closeFlyouts()
-  }
-
-  closeAll(event) {
-    if (event && this.element.contains(event.target)) return
-    sessionStorage.removeItem(STORAGE_KEY)
-    this._closeFlyouts()
-    this.closeAllSheets()
   }
 
   toggleSheet(event) {
